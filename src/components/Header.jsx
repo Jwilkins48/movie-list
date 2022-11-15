1b280d810f51da313c008b2efff29b066749a45e
Jenna <jenna48071@gmail.com>
2022-11-14 22:09:34 -0600

    added header tabs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "../CSS/header.css";
 
 function Header() {
   return (
     <div className="Header-container">
-      <h2>Movie Time</h2>
+      <div className="title-container">
+        <h2>Movie Time</h2>
+        <i class="fa-solid fa-ticket icon"></i>
+      </div>
+
+      <div className="mobile-tab-container">
+        <div className="movies-watched-tab tab">Movies Watched</div>
+        <div className="want-to-watch-tab tab">Want To Watch</div>
+        <div className="spinner-tab tab">Spin</div>
+      </div>
     </div>
   );
 }
